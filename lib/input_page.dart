8fd70cb3d511fbf1ab7b1fe255729d39c6e33f77
toksoykaan <toksoykaan@gmail.com>
2022-08-23 08:53:35 +0300

    Slider added, user can decide the gender with button embedded in containers.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+const bottomActiveColour = Colors.pinkAccent;
+const bottomContainerHeight = 80.0;
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +12,30 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  int height = 180;
+
+  void updateColour(int gender) {
+    if (gender == 1) {
+      if (maleCardColour == inactiveCardColour) {
+        maleCardColour = activeCardColour;
+        femaleCardColour = inactiveCardColour;
+      } else {
+        maleCardColour = inactiveCardColour;
+      }
+    }
+    if (gender == 2) {
+      if (femaleCardColour == inactiveCardColour) {
+        femaleCardColour = activeCardColour;
+        maleCardColour = inactiveCardColour;
+      } else {
+        femaleCardColour = inactiveCardColour;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,36 +43,157 @@ class _InputPageState extends State<InputPage> {
           title: Text('BMI CALCULATOR'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: returnContainer()),
-                  Expanded(child: returnContainer()),
+                  Expanded(
+                      child: GestureDetector(
+                    //Bastın mı naptın onu detect ediyor.
+                    onTap: () {
+                      setState(() {
+                        updateColour(1);
+                      });
+                    },
+                    child: myContainer(
+                      colour: maleCardColour,
+                      cardChild: insideTheBox(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'Male',
+                      ),
+                    ),
+                  )),
+                  Expanded(
+                      child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(2);
+                      });
+                    },
+                    child: myContainer(
+                      colour: femaleCardColour,
+                      cardChild: insideTheBox(
+                          icon: FontAwesomeIcons.venus, label: 'Female'),
+                    ),
+                  )),
                 ],
               ),
             ),
-            Expanded(child: returnContainer()),
+            Expanded(
+                child: myContainer(
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Height'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline
+                        .alphabetic, // 180 ve cm yazısının baseleni eşitliyor
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: TextStyle(
+                            fontSize: 50.0, fontWeight: FontWeight.w900),
+                      ),
+                      Text('cm')
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      thumbColor: Color(0xFFEB1555),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      activeColor: Colors.white,
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        this.setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+              colour: Color(0xFF1D1E33),
+            )),
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: returnContainer()),
-                  Expanded(child: returnContainer()),
+                  Expanded(
+                      child: myContainer(
+                    colour: Color(0xFF1D1E33),
+                  )),
+                  Expanded(
+                      child: myContainer(
+                    colour: Color(0xFF1D1E33),
+                  )),
                 ],
               ),
             ),
+            Container(
+              color: bottomActiveColour,
+              margin: EdgeInsets.only(top: 10),
+              width: double.infinity, //FULL WIDTH veriyor
+              height: bottomContainerHeight,
+            )
           ],
         ));
   }
+}
 
-  Container returnContainer() {
-    var myContainer = Container(
+class insideTheBox extends StatelessWidget {
+  insideTheBox({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+          color: Colors.white,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class myContainer extends StatelessWidget {
+  myContainer({@required this.colour, this.cardChild});
+
+  final Color colour;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10),
       ),
     );
-    return myContainer;
   }
 }
